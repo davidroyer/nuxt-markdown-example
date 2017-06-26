d44907bb0948c31df2d55dc90bd9ebea09100134
David Royer <droyer01@gmail.com>
2017-06-25 23:26:23 -0400

    Added netlify and updated meta title with better info

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'Nuxt Example - Markdown Files',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js Example - Using Markdown Files by David Royer' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
